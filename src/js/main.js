60d9c5eda1ee9c22065c6a4e6010546a1c7ba371
mirrdev <mirrdev@gmail.com>
2022-09-27 15:28:19 +0300

    fix hand logo

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 const submitCheck = document.querySelector("#defaultCheck1");
 const submitButton = document.querySelector("#submit");
 submitCheck.addEventListener("change", ({ target }) => {
-  submitButton.disabled = target.checked;
+  submitButton.disabled = !target.checked;
 });
