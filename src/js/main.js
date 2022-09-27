006f367b690cbd0ae70c17c3d0109701caad45d2
mirrdev <mirrdev@gmail.com>
2022-09-27 12:27:26 +0300

    init main script

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
- 
+const submitCheck = document.querySelector("#defaultCheck1");
+const submitButton = document.querySelector("#submit");
+submitCheck.addEventListener("change", ({ target }) => {
+  submitButton.disabled = target.checked;
+});
